18aa77d348fa55c8ed8500295c12f002a731c7a1
jayel888 <15644@coderacademy.edu.au>
2024-06-27 18:54:29 +1000

    created add restaurant function.

--- restaurant_guide/src/app_operations.py
+++ restaurant_guide/src/app_operations.py
@@ -3,6 +3,25 @@ def  display_restaurants(restaurants):
         for restaurant in restaurants:
             print(f"{restaurant['Restaurant']} - CUISINE: {restaurant['Cuisine']}, ADDRESS: {restaurant['Address']}, {restaurant['Postcode']}, {restaurant['State']}, PH: {restaurant['Phone']}, PRICE: ${restaurant['Est Price PP']}, RATING (Out of 10): {restaurant['Rating']}")
     except KeyError as e:
-        print(f"Error displaying matches: Missing key {e}")
+        print(f"Error displaying restaurants: Missing key {e}")
+    except Exception as e:
+        print(f"An unexpected error occured: {e}")
+
+def add_restaurant(restaurants):
+    try:
+        name = input("What is the name of the restaurant? ")
+        cuisine = input("What type of cuisine? ")
+        address = input("What is the address? ")
+        postcode = input("What is the postcode? ")
+        state = input("What state is it located in? ")
+        phone = input("What is their phone number? ")
+        price = int(input("What is the price per head?"))
+        rating = int(input("What do you rate it? (out of 10): "))
+
+        restaurant = {"Restaurant":name, "Cuisine":cuisine, "Address":address, "Postcode":postcode, "State":state, "Phone":phone, "Est Price PP": price, "Rating":rating }
+        restaurants.append(restaurant)
+        print("Restaurant successfully added.")
+    except ValueError:
+        print("Error: Invalid input. Price and Rating must be an integer.")
     except Exception as e:
         print(f"An unexpected error occured: {e}")
--- restaurant_guide/src/main.py
+++ restaurant_guide/src/main.py
@@ -1,12 +1,14 @@
 from file_operations import import_restaurants_list, save_restaurants_list
-from app_operations import display_restaurants
+from app_operations import display_restaurants, add_restaurant
 
-FILE_PATH = '../data/restaurant_list.json'
+FILE_PATH = 'restaurant_guide/data/restaurant_list.json'
 
 def main():
     restaurants = import_restaurants_list(FILE_PATH)
 
-    print(display_restaurants(restaurants))
+    add_restaurant(restaurants)
+    display_restaurants(restaurants)
+
 
 main ()
 
