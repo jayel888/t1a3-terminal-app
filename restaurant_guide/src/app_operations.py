3950dbddf0cfbb65279d421fe04a43af75a0726a
jayel888 <15644@coderacademy.edu.au>
2024-07-01 21:01:02 +1000

    added error handling for adding a rating

--- restaurant_guide/src/app_operations.py
+++ restaurant_guide/src/app_operations.py
@@ -14,10 +14,13 @@ def add_restaurant(restaurants):
         address = input("What is the address (Enter without State or Postcode, eg. 123 Food Street) ? ")
         postcode = input("What is the postcode? ")
         state = ""
-        if postcode[0] == '2':
-            state = 'NSW'
-        elif postcode[0] =='3':
+
+        if postcode[0] == '3':
             state = 'VIC'
+        elif postcode[0] == '2':
+            state = 'NSW'
+        elif postcode[0] == '0':
+            state = 'NT'
         elif postcode[0] == '4':
             state = 'QLD'
         elif postcode[0] == '5':
@@ -27,11 +30,20 @@ def add_restaurant(restaurants):
         elif postcode[0] == '7':
             state = 'TAS'
         else:
-            state = input("What state is located? ")
+            state = input("Which state is it located in? ")
 
         phone = input("What is their phone number? ")
         price = int(input("What is the price per head? "))
-        rating = int(input("What do you rate it (out of 10)? "))
+    
+        while True:
+            try:
+                rating = int(input("What do you rate it (out of 10)?: "))
+                if rating in range(1,11): 
+                    break
+            except ValueError: pass
+            print("Invalid number. Please enter a number between 1 - 10") 
+            
+
 
         restaurant = {"Restaurant":name, "Cuisine":cuisine, "Address":address, "Postcode":postcode, "State":state, "Phone":phone, "Est Price PP": price, "Rating":rating }
         restaurants.append(restaurant)
